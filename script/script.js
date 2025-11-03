108459ed860ff679899044335a352b067ec40553
Gabrielle Garz <garzgabrielle@gmail.com>
2025-11-03 14:02:17 +0100

    Changement de h3 a p

--- script/script.js
+++ script/script.js
@@ -38,13 +38,12 @@ function createSkillsFromJSON() {
                 const card = document.createElement("div");
                 card.classList.add("col-lg-4", "mt-4");
                 card.innerHTML = `
-                    <div class="card skillsText">
-                        <div class="card-body">
-                            <img src="./images/${item.image}" alt="${item.title}"/>
-                            <h3 class="card-title mt-3">${item.title}</h3>
-                            <p class="card-text mt-3">${item.text}</p>
-                        </div>
-                    </div>
+   <div class="card skillsText">
+        <div class="card-body">
+            <img src="./images/${item.image}" alt="${item.title}"/>
+            <p class="card-title mt-3"><strong>${item.title}</strong></p> <p class="card-text mt-3">${item.text}</p>
+        </div>
+    </div>
                 `;
 
                 // Append the card to the current row
@@ -74,16 +73,12 @@ function createPortfolioFromJSON() {
                 const card = document.createElement("div");
                 card.classList.add("col-lg-4", "mt-4");
                 card.innerHTML = `
-                    <div class="card portfolioContent">
-                    <img class="card-img-top" src="images/${item.image}" alt="${item.title}" style="width:100%">
-                    <div class="card-body">
-                        <h3 class="card-title">${item.title}</h3>
-                        <p class="card-text">${item.text}</p>
-                        <div class="text-center">
-                            <a href="${item.link}" class="btn btn-success" target="_blank" rel="noopener noreferrer">Lien</a>
-                        </div>
-                    </div>
-                </div>
+                   <div class="card skillsText">
+        <div class="card-body">
+            <img src="./images/${item.image}" alt="${item.title}"/>
+            <p class="card-title mt-3"><strong>${item.title}</strong></p> <p class="card-text mt-3">${item.text}</p>
+        </div>
+    </div>
                 `;
 
                 // Append the card to the current row
